36c6bdb5e0a3b2f6f1512dbcf7fd024db06bc1ae
skysky229 <nguyendinhthienhuy@gmail.com>
2023-05-01 16:45:14 +0700

    fix scheduler

--- src/sched.c
+++ src/sched.c
@@ -55,7 +55,7 @@ struct pcb_t * get_mlq_proc(void) {
     for(int i=0;i<MAX_PRIO;i++){
         if((!empty(&mlq_ready_queue[i]))&&slot[i]>0){
             slot[i]--;
-            proc = de_queue(&mlq_ready_queue[i]);
+            proc = dequeue(&mlq_ready_queue[i]);
             break;
         }
     }
